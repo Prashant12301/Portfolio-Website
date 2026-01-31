9caca7c75c7130d495e1895d9489ad028aa09c59
Prashant12301 <prashantyadav12301@gmail.com>
2026-01-31 05:56:42 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,210 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import CloudinaryLogo from './assets/tech_logo/Cloudinary.png';
+
+// Experience Section Logo's
+import ECellLogo from './assets/company_logo/E-Cell.png';
+import GoGreenLogo from './assets/company_logo/green.png';
+
+// Education Section Logo's
+import NitJamshedpurLogo from './assets/education_logo/NitJamshedpur.png';
+import graduationLogo from './assets/education_logo/Graduation.png';
+
+// Project Section Logo's
+
+import portalLogo from './assets/work_logo/portalLogo.png';
+import streamifyLogo from './assets/work_logo/streamifyLogo.png';
+
+
+
+// Skills Information
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Cloudinary', logo: CloudinaryLogo },
+    ],
+  },
+  {
+    title: 'Databases',
+    skills: [
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: ECellLogo,
+      role: "Event Head",
+      company: "E-Cell, NIT Jamshedpur",
+      date: "August 2024 - Present",
+      desc: "As an Event Head at E-Cell, NIT Jamshedpur, I am responsible for planning, organizing, and executing entrepreneurship-focused events, workshops, and competitions aimed at fostering innovation and startup culture on campus. I coordinate with core team members, external speakers, and sponsors to ensure smooth event operations, while managing end-to-end logistics including scheduling, promotions, registrations, and on-ground execution. Through this role, I have led events with 200+ participants, strengthened my leadership and communication skills, and actively contributed to creating an ecosystem that encourages ideation, collaboration, and entrepreneurial thinking among students.",
+      skills: [
+  "Event Management",
+  "Leadership",
+  "Team Coordination",
+  "Communication Skills",
+  "Public Speaking",
+  "Sponsorship Management",
+  "Marketing & Promotions",
+  "Networking",
+  "Problem Solving",
+  "Time Management",
+  "Decision Making",
+  "Strategic Thinking",
+  "Planning & Scheduling",
+  "Entrepreneurship"
+],
+    },
+    {
+      id: 1,
+      img: GoGreenLogo,
+      role: "Creative and Content Head",
+      company: "Go Green Society, NIT Jamshedpur",
+      date: "July 2024 - Present",
+      desc: "As a Creative and Content Head of the Go Green Society at NIT Jamshedpur, I play an active role in conceptualizing and designing visual content to support environmental awareness and sustainability initiatives on campus. My responsibilities include creating posters, banners, social media creatives, and digital assets for events such as plantation drives, cleanliness campaigns, eco-awareness programs, and community outreach activities. I collaborate closely with event coordinators and core team members to develop creative themes, color palettes, and visual narratives that effectively communicate the society’s message. I also contribute to designing promotional materials for both online and offline platforms, ensuring consistency in branding and clarity of communication. Through this role, I have strengthened my creative thinking, design aesthetics, collaboration, and time-management skills while contributing meaningfully to initiatives that promote eco-friendly practices and environmental responsibility among students.",
+      skills: [
+  "Creative Direction",
+  "Content Strategy",
+  "Visual Storytelling",
+  "Graphic Design",
+  "Social Media Content",
+  "Copywriting",
+  "Brand Consistency",
+  "Campaign Planning",
+  "Team Leadership",
+  "Collaboration",
+  "Communication Skills",
+  "Content Review & Editing",
+  "Time Management",
+  "Environmental Awareness"
+],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: NitJamshedpurLogo,
+      school: "National Institute of Technology, Jamshedpur",
+      date: "July 2023 - July 2026",
+      grade: "7.48 CGPA",
+      desc: "I have completed my Master’s degree in Computer Applications (MCA) from National Institute of Technology, Jamshedpur, where I developed a strong academic and practical foundation in computer science and software development. During my time at NIT Jamshedpur, I studied core subjects such as Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering, which strengthened my problem-solving abilities and analytical thinking. The rigorous curriculum, combined with hands-on laboratory work and project-based learning, enabled me to apply theoretical concepts to real-world scenarios. In addition to academics, I actively participated in technical workshops, seminars, and institute-level events, which helped me stay updated with emerging technologies and industry practices. Overall, my experience at NIT Jamshedpur played a vital role in shaping my technical expertise, professional discipline, and readiness for real-world software engineering challenges.",
+      degree: "Master of Computer Applications - MCA",
+    },
+    {
+      id: 1,
+      img: graduationLogo,
+      school: "DAV PG College, Azamgarh",
+      //date: "Sept 2018 - Aug 2021",
+      grade: "6.5 CGPA",
+      desc: "I completed my Bachelor's degree in PCM from DAV PG College, Azamgarh. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of science and technology. I completed my Bachelor’s degree in Physics, Chemistry, and Mathematics (PCM) from DAV PG College, Azamgarh. During my undergraduate studies, I was exposed to a strong foundation in core scientific principles, logical reasoning, and analytical problem-solving. The curriculum emphasized both theoretical knowledge and practical application, which helped me develop a structured and disciplined approach toward learning. Alongside my academic coursework, I cultivated a growing interest in science and technology, particularly in understanding how theoretical concepts translate into real-world applications. I gained practical exposure through academic projects and assignments that required applying mathematical models, scientific reasoning, and logical analysis to solve complex problems. My time at DAV PG College not only strengthened my technical fundamentals but also enhanced my ability to work independently, think critically, and approach challenges with a solution-oriented mindset, which laid a solid foundation for my journey into software development and technology-driven problem solving.I gained practical insights into the world of software development. My time at DAV PG College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Science - BSC (PCM)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Opportunity Job Portal App",
+      description:
+        "A full-stack Job Portal Web Application that connects job seekers with recruiters/companies. The platform supports job posting, job search, applications, authentication, and role-based access using a modern tech stack.",
+      image: portalLogo,
+      tags: ["React JS", "HTML", "CSS", "JavaScript", "REST APIs", "MongoDB", "Node.js", "Express.js"],
+      github: "https://github.com/Prashant12301/Job-Portal-App",
+      webapp: "https://jobportalfrontend-alpha.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "Streamify - Video Calling & Chat App",
+      description:
+        "A full-stack video calling and chat application built with React.js, Node.js, MongoDB, Express.js, Tailwind CSS, and Socket.io. The app allows users to create rooms, join video calls, and chat in real-time with other participants.",
+      image: streamifyLogo,
+      tags: ["React JS", "HTML", "CSS", "JavaScript", "Node.js", "MongoDB", "Express.js", "Tailwind CSS", "Socket.io"],
+      github: "https://github.com/Prashant12301/Streamify_Video_Calling_Chat_App",
+      webapp: "https://streamify-video-calling-chat-app.vercel.app/",
+    },
+  ];  
